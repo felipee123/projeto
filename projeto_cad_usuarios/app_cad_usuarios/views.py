7ee66ab4ccaa9c93dccb499804f7ee6e39e8b88f
FELIPE LEITE <felipeleitedossantos123@gmail.com>
2024-10-29 17:27:54 -0300

    .

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,19 +1,19 @@
 from urllib import request
 from django.shortcuts import render # type: ignore
-from .models import usuario
+from .models import Usuario
 
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
     if request.method == 'POST':
-        novo_usuario = usuario()
+        novo_usuario = Usuario()
         novo_usuario.nome = request.POST.get('nome')
         novo_usuario.sobrenome = request.POST.get('sobrenome')
         novo_usuario.email = request.POST.get('email')
         novo_usuario.save()
 
     # Obter todos os usuários para enviar para o template
-    todos_usuarios = usuario.objects.all()
+    todos_usuarios = Usuario.objects.all()
     
     return render(request, 'usuarios/usuarios.html', {'usuarios': todos_usuarios})
