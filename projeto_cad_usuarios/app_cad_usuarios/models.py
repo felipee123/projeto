ed9125fe787ac10239db1b42366dd38774fc5d78
FELIPE LEITE <felipeleitedossantos123@gmail.com>
2024-10-29 16:50:51 -0300

    sem novidades

--- projeto_cad_usuarios/app_cad_usuarios/models.py
+++ projeto_cad_usuarios/app_cad_usuarios/models.py
@@ -1,6 +1,6 @@
 from django.db import models # type: ignore
 
-class Usuario(models.Model):
+class usuario(models.Model):
     id_aluno = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
     sobrenome = models.TextField(max_length=255)
--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,19 +1,19 @@
 from urllib import request
 from django.shortcuts import render # type: ignore
-from .models import Usuario
+from .models import usuario
 
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
     if request.method == 'POST':
-        novo_usuario = Usuario()
-        novo_usuario.Nome = request.POST.get('nome')
-        novo_usuario.Sobrenome = request.POST.get('sobrenome')
-        novo_usuario.Email = request.POST.get('email')
+        novo_usuario = usuario()
+        novo_usuario.nome = request.POST.get('nome')
+        novo_usuario.sobrenome = request.POST.get('sobrenome')
+        novo_usuario.email = request.POST.get('email')
         novo_usuario.save()
 
     # Obter todos os usuários para enviar para o template
-    todos_usuarios = Usuario.objects.all()
+    todos_usuarios = usuario.objects.all()
     
     return render(request, 'usuarios/usuarios.html', {'usuarios': todos_usuarios})
